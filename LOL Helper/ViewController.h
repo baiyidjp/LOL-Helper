8fd1271879b4116faa57e78a5409dc3724b67243
baiyidjp <tztddong@localhost>
2016-10-09 14:12:26 +0800

    Initial Commit

---
+++ LOL Helper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOL Helper
+//
+//  Created by tztddong on 16/10/9.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
